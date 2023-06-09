db742d42554ea2eac2a2b32e798bba33872f3eb5
Unerkant <freiwifi@gmail.com>
2023-06-09 18:22:27 +0200

    Initial commit

---
+++ src/main/java/com/tictactoc/TicTacToe/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.tictactoc.TicTacToe.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * den 9.6.2023
+ */
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = {"/", "/home"})
+    public String home(){
+
+        return "/home";
+    }
+
+}
